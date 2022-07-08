468d5958361adf70104707557107ef4c2c0d77aa
Mithila Bhandari <mithilabhandari79@gmail.com>
2022-07-08 16:21:04 +0530

    jiranumber

--- AuthorProject/Repositiory/BookRepositiory.cs
+++ AuthorProject/Repositiory/BookRepositiory.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace AuthorProject.Repositiory
 {
+    //Class 
    public class BookRepositiory:AuthorBookContext
     {
         public AuthorBookContext RepObjBook { get; set; }
